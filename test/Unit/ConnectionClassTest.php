e29bdb636ebf75642fc8863fc6dabe6dadb595ef
Rizqon Sadida <rizqoqonsadida@gmail.com>
2019-03-09 19:56:05 +0700

    fix url chaining

--- test/Unit/ConnectionClassTest.php
+++ test/Unit/ConnectionClassTest.php
@@ -5,12 +5,46 @@ use Rizqon\GoogleScrapper\Connection;
 
 class ConnectionClassTest extends TestCase
 {
-    /** @test */
+    /**  */
     public function open_connection_with_style_one()
     {
         $connection = new Connection;
         $result = $connection->get('https://google.com');
 
-        $this->assertIsArray($result);
+        $this->assertIsString($result);
+    }
+
+    /**  */
+    public function open_connection_with_style_two()
+    {
+        $connection = new Connection(
+            [
+                'url' => 'https://google.com'
+            ]
+        );
+        $result = $connection->get();
+
+        $this->assertIsString($result);
+    }
+
+    /**  */
+    public function open_connection_with_style_three()
+    {
+        $connection = new connection;
+        $connection->url = 'https://google.com';
+        $result = $connection->get();
+
+        $this->assertIsString($result);
+    }
+
+    /** @test */
+    public function open_connection_with_style_four()
+    {
+        $connection = new connection;
+        $result = $connection->url('https://google.com')->get();
+
+        var_dump($result);
+
+        $this->assertIsString($result);
     }
 }
