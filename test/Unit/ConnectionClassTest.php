364a0161917b7b7b445cd54f37f7066bf79534fc
Rizqon Sadida <rizqoqonsadida@gmail.com>
2019-03-09 23:06:20 +0700

    add testing with argument

--- test/Unit/ConnectionClassTest.php
+++ test/Unit/ConnectionClassTest.php
@@ -28,6 +28,21 @@ class ConnectionClassTest extends TestCase
     }
 
     /** @test */
+    public function open_connection_with_style_two_with_arguments()
+    {
+        $connection = new Connection(
+            [
+                'url' => 'https://google.com',
+                'proxy' => 'http://rasaf:rasafamily@au.torguardvpnaccess.com:6060',
+                'useragent' => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+            ]
+        );
+        $result = $connection->get();
+
+        $this->assertIsString($result);
+    }
+
+    /** @test */
     public function open_connection_with_style_three()
     {
         $connection = new connection;
@@ -38,10 +53,34 @@ class ConnectionClassTest extends TestCase
     }
 
     /** @test */
+    public function open_connection_with_style_three_with_arguments()
+    {
+        $connection = new connection;
+        $connection->url = 'https://google.com';
+        $connection->proxy = 'http://rasaf:rasafamily@au.torguardvpnaccess.com:6060';
+        $connection->useragent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+        $result = $connection->get();
+
+        $this->assertIsString($result);
+    }
+
+    /** @test */
     public function open_connection_with_style_four()
     {
         $connection = new connection;
         $result = $connection->url('https://google.com')->get();
+
+        $this->assertIsString($result);
+    }
+
+    /** @test */
+    public function open_connection_with_style_four_with_arguments()
+    {
+        $connection = new connection;
+        $result = $connection->url('https://google.com')
+                            ->proxy('http://rasaf:rasafamily@au.torguardvpnaccess.com:6060')
+                            ->useragent('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36')
+                            ->get();
 
         $this->assertIsString($result);
     }
